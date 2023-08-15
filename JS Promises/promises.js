675879eabc00af81f3dce67a1405cdab797d2802
ThicosGroove <thiagottlsilva@gmail.com>
2023-08-15 18:23:04 -0300

    async + await

--- JS Promises/promises.js
+++ JS Promises/promises.js
@@ -1,3 +1,4 @@
+// Using Promises
 const fakeRequestPromise = (url) => {
   return new Promise((resolve, reject) => {
     const delay = Math.floor(Math.random() * 4500) + 500;
@@ -30,3 +31,67 @@ fakeRequestPromise(`yelp.com/api/coffee/page1`)
     console.log("OH NO, A REQUEST FAILED!");
     console.log(error);
   });
+
+// ******************************************************* //
+// Creating my Promise
+
+const delayedColorChange = (color, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      document.body.style.backgroundColor = color;
+      resolve();
+    }, delay);
+  });
+};
+
+// Forma de usar async em cascata sem palavra-chave await
+// delayedColorChange(`red`, 1000)
+//   .then(() => delayedColorChange(`red`, 1000))
+//   .then(() => delayedColorChange(`orange`, 1000))
+//   .then(() => delayedColorChange(`yellow`, 1000))
+//   .then(() => delayedColorChange(`green`, 1000))
+//   .then(() => delayedColorChange(`blue`, 1000))
+//   .then(() => delayedColorChange(`indigo`, 1000))
+//   .then(() => delayedColorChange(`violet`, 1000));
+
+// ******************************************************* //
+// Async Await Keywords
+// Melhor forma de usar
+// Combinar Async e Await
+
+async function rainbow() {
+  await delayedColorChange(`red`, 1000);
+  await delayedColorChange(`orange`, 1000);
+  await delayedColorChange(`yellow`, 1000);
+  await delayedColorChange(`green`, 1000);
+  await delayedColorChange(`blue`, 1000);
+  await delayedColorChange(`indigo`, 1000);
+  await delayedColorChange(`violet`, 1000);
+  return "ALL DONE!";
+}
+
+//rainbow().then(() => console.log("END OF RAINBOW!"));
+
+async function printRainbow() {
+  await rainbow();
+  console.log("END OF RAINBOW!");
+}
+
+printRainbow();
+
+// ************************************************************** //
+// Melhor forma de usar
+
+async function makeTwoRequests() {
+  try {
+    let data1 = await fakeRequestPromise(`/page1`);
+    console.log(data1);
+    let data2 = await fakeRequestPromise(`/page2`);
+    console.log(data2);
+  } catch (e) {
+    console.log("CAUGHT AAN ERROR");
+    console.log("Error is: ", e);
+  }
+}
+
+makeTwoRequests();
