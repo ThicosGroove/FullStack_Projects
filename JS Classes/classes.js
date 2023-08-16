4cdb91854661d6cd591f62fe7a2d778251f4947a
ThicosGroove <thiagottlsilva@gmail.com>
2023-08-16 15:09:12 -0300

    classes in JS

---
+++ JS Classes/classes.js
@@ -0,0 +1,24 @@
+class Color {
+  constructor(r, g, b, name) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.name = name;
+  }
+
+  innerRGB() {
+    const { r, g, b } = this;
+    return `${r}, ${g}, ${b}`;
+  }
+
+  rgb() {
+    return `rgb(${this.innerRGB()})`;
+  }
+
+  rgba(a = 1.0) {
+    return `rgba(${this.innerRGB}, ${a})`;
+  }
+}
+
+const red = new Color(255, 67, 89, "tomato");
+const whit = new Color(255, 255, 255, "white");
