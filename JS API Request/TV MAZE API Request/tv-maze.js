e548eb9cf17a55192d86d2bbe93983fe50942419
ThicosGroove <thiagottlsilva@gmail.com>
2023-08-16 11:04:19 -0300

    api request tv maze better request

---
+++ JS API Request/TV MAZE API Request/tv-maze.js
@@ -0,0 +1,25 @@
+const form = document.querySelector("#search-form");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  try {
+    const searchTerm = form.elements.query.value;
+    const config = { params: { q: searchTerm } };
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    makeImages(res.data);
+
+    form.elements.query.value = "";
+  } catch (e) {
+    console.log("ERROR!", e);
+  }
+});
+
+const makeImages = (shows) => {
+  for (let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement(`IMG`);
+      img.src = result.show.image.medium;
+      document.body.append(img);
+    }
+  }
+};
